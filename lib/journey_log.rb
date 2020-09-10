180f6999ceccaf1d48b470cdb373e02a191975cf
Xavier Defontaine <xdefontaine@gmail.com>
2020-09-10 15:20:14 +0100

    (WIP) adding currenct journey

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,12 +1,17 @@
 class JourneyLog
 
+
     def initialize(journey_class = Journey.new)
       @journey = journey_class
+      @journeys = []
     end
 
     def start(station)
       @journey.start(station) 
     end
 
+    def current_journey
+      @journey
+    end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -5,4 +5,11 @@ describe JourneyLog do
   it "Check that journey start equals a new journey object" do
     expect(subject.start(station)).to eq station
   end
+  
+  let(:journey_class) { double :journey_class, new: journey}
+
+  it "Return an incomplete journey or create a new journey" do
+    expect(subject.current_journey).to eq journey
+ end
+
 end
